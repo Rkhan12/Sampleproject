b83bee404150152729dd0a391071e9c25fe25d97
Ninesol <RKhan12>
2017-02-10 12:32:32 +0500

    change again

--- app/src/main/java/com/example/ninesol/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/ninesol/sampleproject/MainActivity.java
@@ -3,18 +3,30 @@ package com.example.ninesol.sampleproject;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
-
+public Button button;
+public TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 // why are you deleting this
         // Raheel side
         //Cast views here
+        button=(Button)findViewById(R.id.button);
+        tv=(TextView)findViewById(R.id.tv);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             tv.setText("Hello there");
+            }
+        });
     }
 
 
