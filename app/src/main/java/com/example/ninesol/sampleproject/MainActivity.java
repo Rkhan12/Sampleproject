4b82a96782c702003395d5bc33e3f20333ed3700
Ninesol <RKhan12>
2017-02-10 12:20:23 +0500

    change again

--- app/src/main/java/com/example/ninesol/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/ninesol/sampleproject/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 // why are you deleting this
+        // Raheel side
     }
 
 
