d66857643e3db0e0b137896913816fab62cce6d5
RKhan12 <raheelzafar.cdz@gmail.com>
2017-02-10 13:10:56 +0500

    text

--- app/src/main/java/com/example/ninesol/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/ninesol/sampleproject/MainActivity.java
@@ -21,13 +21,13 @@ public TextView tv;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             tv.setText("Farhan side");
+             tv.setText("Good Morning");
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText("Raheel side");
+                tv.setText("Good Night");
             }
         });
     }
